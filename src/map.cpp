5f70d128f1cccc26cae2851bb218df7e439e48cc
AmirrezaDaei <amir2006daei@gmail.com>
2025-05-10 20:29:04 +0330

    Redesign drawBalloons method in Map

--- src/map.cpp
+++ src/map.cpp
@@ -34,7 +34,7 @@ map_width(map_width_), map_height(map_height_), texture_manager(texture_manager_
 
 void Map::constructBalloons(Vector2f position) {
     Texture &texture = texture_manager->getTexture(BALLOON_FILENAME);
-    shared_ptr<Balloon> new_balloon = make_shared<Balloon>(texture, position, Vector2i(1, 0), 75, 2);
+    shared_ptr<Balloon> new_balloon = make_shared<Balloon>(texture, position, Vector2i(1, 0), 150, 2);
     balloons.push_back(new_balloon);
 }
 
@@ -45,19 +45,25 @@ void Map::drawTiles(RenderWindow &window)
 }
 
 void Map::drawBalloons(RenderWindow &window, float dt) {
-    for (auto& balloon : balloons) {
+    for(auto it = balloons.begin(); it != balloons.end();) {
+        auto& balloon = *it;
         balloon->move(dt);
         Vector2f pos = balloon->getPosition();
         Vector2i current_dir = balloon->getVDir();
-            if (!this->isPath(pos, current_dir)) {
-                Vector2i right_dir = Vector2i(-current_dir.y, current_dir.x);
-                Vector2i left_dir  = Vector2i(current_dir.y, -current_dir.x);
-                if (this->isPath(pos, right_dir))
-                    balloon->turnRight();
-                else if (this->isPath(pos, left_dir))
-                    balloon->turnLeft();
+        if(!this->isPath(pos, current_dir)) {
+            Vector2i right_dir = Vector2i(-current_dir.y, current_dir.x);
+            Vector2i left_dir  = Vector2i(current_dir.y, -current_dir.x);
+            if (this->isPath(pos, right_dir))
+                balloon->turnRight();
+            else if (this->isPath(pos, left_dir))
+                balloon->turnLeft();
+            else {
+                it = balloons.erase(it);
+                continue;
             }
+        }
         balloon->draw(window);
+        ++it;
     }
 }
 
